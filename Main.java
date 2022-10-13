2ea4bb3286950815be048f832b4b576468489264
LivanMDC <113356765+LivanMiranda@users.noreply.github.com>
2022-10-13 17:20:03 +0000

    satisfactory execution!

--- Main.java
+++ Main.java
@@ -4,11 +4,52 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException {
+    ArrayList<String> arrayList = new ArrayList<>();
+    try {
 
-		ArrayList<String> arrayList = new ArrayList<>();
-		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
-		
-	}
-}
+      FileReader fr = new FileReader("names.txt");
+      Scanner fileScanner = new Scanner(fr);
+      while (fileScanner.hasNextLine()) {
+        arrayList.add(fileScanner.nextLine());
+
+      }
+    fileScanner.close();
+    } catch (FileNotFoundException error) {
+
+    }
+    System.out.println("Press 1 to learn about salary.");
+    System.out.println("Press 2 to learn about the job.");
+    System.out.println("Press 3 to learn about demand.");
+    System.out.println("Press 4 to view current students.");
+    System.out.println("Press 5 to quit.");
+    
+    Scanner option = new Scanner(System.in);
+   int x = option.nextInt();
+    while (x< 5) {
+      
+      if (x == 1) {
+        System.out.println("$98,345 average salary in South Florida!");
+      }
+      if (x == 2) {
+        System.out.println("US News - 100 Best Jobs!");
+      }
+      if (x == 3) {
+        System.out.println("Top 10 Forbes In-Demand Jobs!");
+      }
+      if (x == 4) {
+        for (int i = 0; i < arrayList.size(); i++) {
+          System.out.println(arrayList.get(i));
+
+        }
+      }
+      
+    option = new Scanner(System.in);
+      x = option.nextInt();
+    }
+      option.close();
+    System.exit(0);
+    }
+  }
+
+
